a40e8ca782187a4edde8e43f61f1556499e79174
Michael Nisi <michael.nisi@gmail.com>
2021-02-24 07:11:14 +0100

    Recover previews

--- Sources/Clay/Slider.swift
+++ Sources/Clay/Slider.swift
@@ -106,3 +106,102 @@ private extension Slider {
     return CGFloat(result)
   }
 }
+
+// MARK: - Preview
+
+struct Preview: View {
+  
+  @State var value: Double = 30
+  
+  var background: Color {
+    Color(red: 0.07, green: 0.07, blue: 0.12)
+  }
+  
+  var simple: some View {
+    Slider(value: $value,  range: (0, 100)) { modifiers, value in
+      ZStack {
+        Color.blue.cornerRadius(3).frame(height: 6).modifier(modifiers.barLeft)
+        Color.blue.opacity(0.2).cornerRadius(3).frame(height: 6).modifier(modifiers.barRight)
+        ZStack {
+          Circle().fill(Color.white)
+          Circle().stroke(Color.black.opacity(0.2), lineWidth: 2)
+        }.modifier(modifiers.knob)
+      }
+    }.frame(height: 25)
+  }
+  
+  var track: some View {
+    Slider(value: $value, range: (0, 100), knobWidth: 0) { modifiers, value in
+      ZStack {
+        ZStack {
+          Color.purple.modifier(modifiers.barLeft)
+          Color.gray.modifier(modifiers.barRight)
+          HStack {
+            Text(("\(Int(self.value))")).font(.body).padding(.leading)
+            Spacer()
+            Text(("100")).font(.body).padding(.trailing)
+          }.foregroundColor(.white)
+        }.cornerRadius(.zero)
+      }.cornerRadius(15)
+    }.frame(height: 30)
+  }
+  
+  var textOverlay: some View {
+    Slider(value: $value, range: (0, 100), knobWidth: 0) { modifiers, value in
+      ZStack {
+        background
+        LinearGradient(gradient: .init(colors: [background, Color.black.opacity(0.6) ]), startPoint: .bottom, endPoint: .top)
+        
+        Group {
+          LinearGradient(gradient: .init(colors: [Color.blue, Color.purple, Color.pink ]), startPoint: .leading, endPoint: .trailing)
+          LinearGradient(gradient: .init(colors: [Color.clear, background ]), startPoint: .top, endPoint: .bottom).opacity(0.15)
+        }.modifier(modifiers.barLeft)
+        
+        Text("Custom Slider").foregroundColor(.white)
+      }
+      .cornerRadius(8)
+    }
+    .frame(height: 40)
+    .padding(2)
+    .background(
+      // adds shadow border around entire slider (to make it appear inset)
+      LinearGradient(gradient: .init(colors: [Color.gray, Color.black ]), startPoint: .bottom, endPoint: .top)
+        .opacity(0.2)
+        .cornerRadius(9)
+    )
+  }
+  
+  var apple: some View {
+    SwiftUI.Slider(value: $value)
+  }
+  
+  var button: some View {
+    Button("Random Change") {
+      withAnimation {
+        value = .random(in: 0...100)
+      }
+    }
+  }
+  
+  var body: some View {
+    return ZStack {
+      background.edgesIgnoringSafeArea(.all)
+      VStack(spacing: 30) {
+        Group {
+          simple
+          track
+          textOverlay
+          apple
+        }
+        button
+        .frame(width:320)
+      }
+    }
+  }
+}
+
+struct TrackSlider_Previews: PreviewProvider {
+  static var previews: some View {
+    Preview()
+  }
+}
